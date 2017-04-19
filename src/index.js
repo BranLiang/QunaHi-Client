9ffb6dee65c7de5b342d135a702230e3c8f996d1
BranLiang <lby89757@hotmail.com>
2017-04-19 23:31:55 +0800

    add container folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Qunahi from './containers/qunahi';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Qunahi />,
   document.getElementById('root')
 );
