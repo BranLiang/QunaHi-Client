ea9833c7cbe0473191df024599b0587c2531f318
BranLiang <lby89757@hotmail.com>
2017-04-20 15:01:43 +0800

    remove the logger package for the moment as it has breaking change on new version

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import 'babel-polyfill';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { browserHistory } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
+// import { browserHistory } from 'react-router';
+// import { syncHistoryWithStore } from 'react-router-redux';
 import configureStore from './store/configureStore';
 import Qunahi from './containers/QunaHi';
 import './index.css';
 
+const store = configureStore();
+// const history = syncHistoryWithStore(browserHistory, store);
+
 ReactDOM.render(
-  <Qunahi />,
+  <Qunahi
+    store={store}
+  />,
   document.getElementById('root')
 );
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,17 +1,14 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import createLogger from 'redux-logger';
 import rootSaga from '../sagas';
 import rootReducer from '../reducers';
 import DevTools from '../containers/DevTools';
 
 export default function configureStore(initialState) {
   const sagaMiddleware = createSagaMiddleware();
-  const logger = createLogger();
 
   const middlewares = [
     sagaMiddleware,
-    process.env.NODE_ENV === 'development' && logger,
   ].filter(Boolean);
 
 
