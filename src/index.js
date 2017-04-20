a65e800d01d28f589e00dc94a09c8b9e9dc85a4d
BranLiang <lby89757@hotmail.com>
2017-04-20 22:57:02 +0800

    remove react-router-redux

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import 'babel-polyfill';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { browserHistory } from 'react-router';
-// import { syncHistoryWithStore } from 'react-router-redux';
+import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import Qunahi from './containers/QunaHi';
 import './index.css';
